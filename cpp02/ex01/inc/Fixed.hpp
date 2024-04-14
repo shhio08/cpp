ca8b70c71e2e9e8770cc9f711007a85be157266f
shhio08 <kiiro18.sh@gmail.com>
2024-04-14 20:01:56 +0900

    umakuikanai

--- cpp02/ex01/inc/Fixed.hpp
+++ cpp02/ex01/inc/Fixed.hpp
@@ -16,15 +16,9 @@ public:
 	Fixed &operator=(const Fixed &other);
 	~Fixed();
 	Fixed(const int int_val);
-	// 定数整数をパラメータとして受け取るコンストラクタ。
-	// これを対応する固定小数点値に変換します。小数ビットの値は、練習問題 00 のように 8 に初期化されます
 	Fixed(const float float_val);
-	// 定数の浮動小数点数をパラメータとして受け取るコンストラクタ。
-	// これは、対応する固定小数点値に変換します。小数ビットの値は、練習問題00のように8に初期化されます。
-	float toFloat(void) const;
-	// 固定小数点値を浮動小数点値に変換するメンバ関数
-	int toInt(void) const;
-	//固定小数点値を整数値に変換するメンバ関数
+	float toFloat() const;
+	int toInt() const;
 	int getRawBits() const;
 	void setRawBits(int const raw);
 
